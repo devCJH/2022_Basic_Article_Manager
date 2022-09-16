98aea56eae3cc1985dbc813cfebfbf6e19387a10
devCJH <http-@naver.com>
2022-09-16 20:37:03 +0900

    게시물 삭제 기능 구현

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -54,56 +54,55 @@ public class Main {
 					System.out.printf("%d	|	%s\n", article.id, article.title);
 				}
 			} else if (cmd.startsWith("article detail ")) {
-				
+
 				String[] cmdBits = cmd.split(" ");
 				int id = Integer.parseInt(cmdBits[2]);
-				
+
 				Article foundArticle = null;
-				
-				for(int i = 0; i < articles.size(); i++) {
+
+				for (int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
-					
-					if(article.id == id) {
+
+					if (article.id == id) {
 						foundArticle = article;
 						break;
 					}
 				}
-				
-				if(foundArticle == null) {
+
+				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
 					continue;
-				} else {
-					System.out.printf("번호 : %d\n", foundArticle.id);
-					System.out.printf("날짜 : %s\n", "2022-12-12 12:12:12");
-					System.out.printf("제목 : %s\n", foundArticle.title);
-					System.out.printf("내용 : %s\n", foundArticle.body);
 				}
-				
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", "2022-12-12 12:12:12");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+
 			} else if (cmd.startsWith("article delete ")) {
-				
+
 				String[] cmdBits = cmd.split(" ");
 				int id = Integer.parseInt(cmdBits[2]);
-				
-				Article foundArticle = null;
-				
-				for(int i = 0; i < articles.size(); i++) {
+
+				int foundIndex = -1;
+
+				for (int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
-					
-					if(article.id == id) {
-						foundArticle = article;
+
+					if (article.id == id) {
+						foundIndex = i;
 						break;
 					}
 				}
-				
-				if(foundArticle == null) {
+
+				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
 					continue;
-				} 
-				
-				articles.remove(id - 1);
-				
+				}
+
+				articles.remove(foundIndex);
+
 				System.out.printf("%d번 게시물이 삭제되었습니다\n", id);
-				
+
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
